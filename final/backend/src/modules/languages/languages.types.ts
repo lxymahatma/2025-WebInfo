98930580ec13b42ae1b5637210e4b5bff0c1ae6d
lxymahatma <lxymahatma@gmail.com>
2025-07-27 21:37:17 +0800

    fix language

--- final/backend/src/modules/languages/languages.types.ts
+++ final/backend/src/modules/languages/languages.types.ts
@@ -2,7 +2,8 @@ import type { TranslationKeys } from "@eduplayground/shared/types/language";
 
 export interface LanguageDB {
   translations: {
-    en: TranslationKeys;
-    jp: TranslationKeys;
+    en_US: TranslationKeys;
+    ja_JP: TranslationKeys;
+    zh_CN: TranslationKeys;
   };
 }
